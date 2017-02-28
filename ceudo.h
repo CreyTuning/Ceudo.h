0ec8107a860b5075b69b6874958bf022188eec5e
creytuning <creytuning@hotmail.com>
2017-02-28 16:18:24 -0400

    Organizacion del codigo
    mejoras en el codigo

--- ceudo.h
+++ ceudo.h
@@ -3,41 +3,46 @@
 #include <iostream>
 
 //ESTRUCTURAS
-#define inicio int main(int argc,char *argv[]){
-#define fin return 0; }
-#define INICIO int main(int argc,char *argv[]){
-#define FIN return 0; }
+#define inicio 				int main(int argc,char *argv[]){
+#define fin 				return 0;}
+#define INICIO 				int main(int argc,char *argv[]){
+#define FIN 				return 0;}
 
 //TIPOS DE DATOS
-#define entero int
-#define real float
-	#define largo long
-	#define corto short
-#define logico bool
-#define caracter char
-#define cadena char
-	#define de(x) [x];
+#define entero 				int
+#define real 				float
+#define logico 				bool
+#define caracter 			char
+#define cadena 				char
+#define de(x) 				[x];
+#define largo 				long
+#define corto 				short
 
 //FUNCIONES
-#define escribir(x) std::cout<<x;
-#define leer(x) std::cin>>x;
+#define escribir(x) 		std::cout<<x;
+#define leer(x) 			std::cin>>x;
 
 //FACILIDADES
-#define es =
-#define igual ==
-#define mayor >
-#define menor <
-#define menorigual <=
-#define mayorigual >=
-#define diferente !=
-#define VERDADERO TRUE
-#define FALSO FALSE
+#define es 					=
+#define igual 				==
+#define mayor 				>
+#define menor 				<
+#define menorigual 			<=
+#define mayorigual 			>=
+#define diferente 			!=
+#define VERDADERO 			TRUE
+#define FALSO 				FALSE
+#define mas					+
+#define menos				-
+#define por					*
+#define entre 				/
 
 //CONDICIONALES
-#define si(x) if(x)
-#define sino(x) else if(x)
-#define no else
-#define entonces {
-#define fsi }
-#define fsino }
-#define fsino }
+#define si(x) 				if(x)
+#define sino(x) 			else if(x)
+#define no 					else
+#define entonces 			{
+#define fentonces 			}
+#define fsi 				}
+#define fsino 				}
+#define fsino 				}
