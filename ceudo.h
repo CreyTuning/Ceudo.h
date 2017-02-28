b738e7e428625dfadafa330cf27ac1aa0654c083
creytuning <creytuning@hotmail.com>
2017-02-28 17:22:57 -0400

    mas arreglos

--- ceudo.h
+++ ceudo.h
@@ -10,17 +10,23 @@
 
 //TIPOS DE DATOS
 #define entero 				int
+#define enteros 			int
 #define real 				float
+#define reales 				float
 #define logico 				bool
+#define logicos 			bool
 #define caracter 			char
+#define caracteres 			char
 #define cadena 				char
-#define de(x) 				[x];
+#define cadenas 			char
+#define de(x) 				[x]
 #define largo 				long
 #define corto 				short
 
 //FUNCIONES
 #define escribir(x) 		std::cout<<x;
 #define leer(x) 			std::cin>>x;
+#define leerc(x) 			cin.get(x);
 
 //FACILIDADES
 #define es 					=
@@ -30,19 +36,34 @@
 #define menorigual 			<=
 #define mayorigual 			>=
 #define diferente 			!=
-#define VERDADERO 			TRUE
-#define FALSO 				FALSE
+#define VERDAD	 			1
+#define VERDADERO	 		1
+#define FALSO 				0
+#define verdad	 			1
+#define verdadero	 		1
+#define falso 				0
 #define mas					+
 #define menos				-
 #define por					*
 #define entre 				/
+#define y 					<<
+#define crece				++;
+#define decrece				--;
 
 //CONDICIONALES
-#define si(x) 				if(x)
-#define sino(x) 			else if(x)
-#define no 					else
-#define entonces 			{
-#define fentonces 			}
+#define si(x)				if(x)
+#define sino(x)				else if(x)
+#define no					else
 #define fsi 				}
 #define fsino 				}
-#define fsino 				}
+#define fno 				}
+	//GLOBAL
+	#define entonces 		{
+	#define fentonces 		}
+
+//BUCLES
+#define mientras(x)			while(x)
+#define fmientras			}
+
+//DECORACION
+#define SL					std::cout<<"\n";
